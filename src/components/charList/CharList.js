1985556ccf82dd2c8857b3f00f62046880c31941
Ildar <ildarvagapov528@gmail.com>
2023-06-03 18:38:37 +0500

    f

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,9 +1,6 @@
 import { Component } from 'react';
 import { MarvelService } from '../../services/MarvelService';
-
 import './charList.scss';
-// import Spinner from '../spinner/Spinner';
-// import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class CharList extends Component {
 
@@ -24,32 +21,24 @@ class CharList extends Component {
 		})
 	}
 
-	renderItems(arr) {
-		return (
-			<ul className="char__grid">
-				{
-					arr.map((item) => {
-						return (
-							<li
-								className="char__item"
-								key={item.id}>
-								<img src={item.thumbnail} alt={item.name} />
-								<div className="char__name">{item.name}</div>
-							</li>
-						)
-					})
-				}
-			</ul>
-		)
-	}
-
 	render() {
-		const { charList } = this.state;
-		const content = this.renderItems(charList);
-
 		return (
 			<div className="char__list">
-				{content}
+				<ul className="char__grid">
+					{
+						// мапим
+						this.state.charList.map((item) => {
+							return (
+								<li
+									className="char__item"
+									key={item.id}>
+									<img src={item.thumbnail} alt={item.name} />
+									<div className="char__name">{item.name}</div>
+								</li>
+							)
+						})
+					}
+				</ul>
 				<button className="button button__main button__long">
 					<div className="inner">load more</div>
 				</button>
